dc4461b720f25f82e2d7f5bcd0405f8592c60fea
Kevin Courtney <kevincourtney@gmail.com>
2019-10-10 21:15:48 -0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'themes';
+  theme: string = "my-light-theme";
+
+  constructor(
+   private overlayContainer: OverlayContainer
+  ) {}
+
+  ngOnInit(): void {
+    //this.overlayContainer.themeClass = this.theme;
+    //console.log(this.overlayContainer);
+    this.overlayContainer.getContainerElement().classList.add(this.theme);
+    console.log(this.overlayContainer.getContainerElement().classList);
+  }
+
+  onThemeChange(theme:string) {
+   
+   this.theme=theme;
+
+   const overlayContainerClasses = this.overlayContainer.getContainerElement().classList;
+   const themeClassesToRemove = Array.from(overlayContainerClasses).filter((item: string) => item.includes('-theme'));
+   if (themeClassesToRemove.length) {
+     overlayContainerClasses.remove(...themeClassesToRemove);
+   }
+   overlayContainerClasses.add(theme);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatButtonModule, MatSelectModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule, 
+    MatButtonModule, 
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
